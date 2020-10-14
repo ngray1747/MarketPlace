8395f2a088ca9683e58b5647bb5a33be7cf48188
Hải minh <hailmdev169@gmail.com>
2020-10-14 17:33:01 +0700

    update

--- MarketPlace/src/main/java/com/modul/marketplace/ToastShow.kt
+++ MarketPlace/src/main/java/com/modul/marketplace/ToastShow.kt
@@ -3,7 +3,6 @@ package com.modul.marketplace
 import android.content.Context
 import android.widget.Toast
 
-
 object ToastShow {
     fun showMessage(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
