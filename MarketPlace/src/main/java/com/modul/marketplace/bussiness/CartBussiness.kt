2fcc58f5883a40c5dfba275443e53c685c047380
Hải minh <hailmdev169@gmail.com>
2020-10-21 16:29:10 +0700

    Update CartBussiness.kt

--- MarketPlace/src/main/java/com/modul/marketplace/bussiness/CartBussiness.kt
+++ MarketPlace/src/main/java/com/modul/marketplace/bussiness/CartBussiness.kt
@@ -101,9 +101,10 @@ class CartBussiness {
         }
         nvlOnline.final_amount = mOrderModel.amount
         nvlOnline.is_red_invoice = mOrderModel.requestInvoice
+
         nvlOnline.customer_id = userId
         nvlOnline.customer_name = userId
-        nvlOnline.recipient_name = mOrderModel.contactName
+        nvlOnline.recipient_name = userId
         nvlOnline.recipient_id = mOrderModel.dmDeliveryInfo?.receiverPhone
         nvlOnline.company_id = companyId
 
