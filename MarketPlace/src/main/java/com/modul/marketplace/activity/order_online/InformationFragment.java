21d1af101f365b2cf8bd999664fd39f47a88cd5b
Hải minh <hailmdev169@gmail.com>
2020-11-19 17:26:39 +0700

    update

--- MarketPlace/src/main/java/com/modul/marketplace/activity/order_online/InformationFragment.java
+++ MarketPlace/src/main/java/com/modul/marketplace/activity/order_online/InformationFragment.java
@@ -172,8 +172,10 @@ public class InformationFragment extends BaseFragment {
             }
         });
         mPayment.setOnClickListener(v1 -> {
-            mCartBussiness.getOrder();
-            if (mCartBussiness.getOrder().getOrderCode() != null) {
+//            mCartBussiness.getOrder();
+            Log.i(TAG, "getOrderCode: " + mCartBussiness.getOrder().getOrderCode());
+
+            if (!TextUtils.isEmpty(mCartBussiness.getOrder().getOrderCode())) {
                 if (DmOrderOnline.ZALOPAY.equals(typePayment)) {
                     apiZaloPaymentCreate(mCartBussiness.getOrder());
                 } else {
